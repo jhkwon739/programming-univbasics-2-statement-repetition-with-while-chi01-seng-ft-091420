5a4b92693b5f4042d6c737496b6ac45a2c19e87b
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-10 02:32:33 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -5,7 +5,8 @@ count = 10
 
 #decrement count by 1 each time and print count and when it gets to zero end loop and print message
 while count > 0 do 
-  puts "#{count}"
+  #puts "#{count}"
+  puts count
   count -= 1 
 end
 puts "Happy New Year!"
