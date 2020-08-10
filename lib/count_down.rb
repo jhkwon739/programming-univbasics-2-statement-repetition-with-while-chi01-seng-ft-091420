35e7fa364a738258728001e41acac1ecc8f6b431
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-10 02:31:42 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -3,6 +3,8 @@
 #start countdown from 10
 count = 10
 
+
+#decrement count by 1 each time and print count and when it gets to zero end loop and print message
 while count > 0 do 
   puts "#{count}"
   count -= 1 
